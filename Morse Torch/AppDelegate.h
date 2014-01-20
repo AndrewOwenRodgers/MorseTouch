7690770493a655f01577df7b8039ba4ac95c5c78
AndrewOwenRodgers <owenrhyfelwr@gmail.com>
2014-01-20 10:19:51 -0800

    Initial Commit

---
+++ Morse Torch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Morse Torch
+//
+//  Created by Andrew Rodgers on 1/20/14.
+//  Copyright (c) 2014 Andrew Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Morse Torch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Morse Torch
+//
+//  Created by Andrew Rodgers on 1/20/14.
+//  Copyright (c) 2014 Andrew Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
